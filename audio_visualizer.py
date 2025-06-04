778567fb60b3289c157bd16835b848f6c6513368
Morellonom1con <bhargavsadari32@gmail.com>
2025-06-05 01:53:20 +0530

    initial GUI done

--- audio_visualizer.py
+++ audio_visualizer.py
@@ -1,9 +1,9 @@
-from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton,QFrame,QSlider,QLabel,QOpenGLWidget
-from PyQt5.QtCore import Qt,QPropertyAnimation,QPoint,QTimer
+from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton,QStyle,QFrame,QSlider,QLabel,QOpenGLWidget
+from PyQt5.QtCore import Qt,QPropertyAnimation,QPoint,QTimer,QUrl,QTime
 from PyQt5.QtGui import QPainter,QColor,QFont,QPolygon
+from PyQt5.QtMultimedia import QMediaPlayer,QMediaContent
 from OpenGL.GL import *
 import numpy as np
-from random import *
 import sys
 import time
 
@@ -34,9 +34,18 @@ class TrapezoidButton(QPushButton):
         painter.setPen(QColor(255,255,255))
         painter.setFont(QFont("Arial",12,QFont.Bold))
         if(self.Flipped):
-            painter.drawText(self.rect(),Qt.AlignCenter,">")
+            painter.drawText(self.rect(),Qt.AlignCenter,"›")
         else:
-            painter.drawText(self.rect(),Qt.AlignCenter,"<")    
+            painter.drawText(self.rect(),Qt.AlignCenter,"‹")    
+
+class ClickableSlider(QSlider):
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            val = QStyle.sliderValueFromPosition(self.minimum(), self.maximum(), event.x(), self.width())
+            self.setValue(val)
+            self.sliderMoved.emit(val)
+        super().mousePressEvent(event)
+
 
 class GLviewport(QOpenGLWidget):
     def __init__(self,parent=None):
@@ -85,11 +94,11 @@ class GLviewport(QOpenGLWidget):
         void main()
         {
             float spin_time = u_time;
-            float SPIN_EASE = 0.4;
+            float SPIN_EASE = 0.4; // controlls strength of swirl  
 
-            vec2 screen_coords = uv * u_resolution;
+            vec2 screen_coords = uv * u_resolution; // to scale properly
 
-            vec2 pos = (screen_coords - 0.5 * u_resolution) / length(u_resolution) - vec2(0.0, 0.0);
+            vec2 pos = (screen_coords - 0.5 * u_resolution) / length(u_resolution) - vec2(0.0, 0.0); // last part is the center offset
             float uv_len = length(pos);
 
             float speed = (spin_time * SPIN_EASE * 0.2) + 302.2;
@@ -159,9 +168,9 @@ class GLviewport(QOpenGLWidget):
         glUniform2f(glGetUniformLocation(self.shader_program, "u_resolution"), self.width(), self.height())
 
         # your 3 color inputs
-        glUniform4f(glGetUniformLocation(self.shader_program, "u_colour_1"), 0.5, 1.0, 0.9, 1.0)#outer color
+        glUniform4f(glGetUniformLocation(self.shader_program, "u_colour_1"), 0.5, 1.0, 0.7, 1.0)#outer color
         glUniform4f(glGetUniformLocation(self.shader_program, "u_colour_2"), 0.1, 0.1, 0.1, 1.0)#center color
-        glUniform4f(glGetUniformLocation(self.shader_program, "u_colour_3"), 0.0, 0.2, 1.0, 1.0)#dominant middle color
+        glUniform4f(glGetUniformLocation(self.shader_program, "u_colour_3"), 0.0, 0.2, 0.9, 1.0)#dominant middle color
 
         glBindVertexArray(self.VAO)
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
@@ -179,6 +188,10 @@ class MainWindow(QMainWindow):
         self.setGeometry(500,250,800,600)
         
         self.viewport=GLviewport(self)
+        self.music_player=QMediaPlayer(self)
+        self.music_player.setVolume(50)
+        self.music_player.setMedia(QMediaContent(QUrl.fromLocalFile("/home/bhargav/Downloads/Syn Cole - Feel Good.mp3")))
+
 
         self.toggle_btn=TrapezoidButton(self)
         self.toggle_btn.move(self.width()-self.toggle_btn.width(),int(self.height()/2)-50)
@@ -190,17 +203,70 @@ class MainWindow(QMainWindow):
         self.side_menu.setStyleSheet("background-color: #444;")
 
         self.player_menu=QFrame(self)
+
         self.play_button=QPushButton(self.player_menu)
-        self.play_slider=QSlider(Qt.Horizontal,self.player_menu)
-        self.play_time=QLabel("00:00 / 00:00",self.player_menu)
+        self.play_button.setText("⏵")
+        self.play_button.clicked.connect(self.toggle_play)
+        self.play_button.setStyleSheet("font-size: 14px;color:white;font-weight:bold")
+
+        self.play_slider=ClickableSlider(Qt.Horizontal,self.player_menu)
+        self.play_slider.sliderMoved.connect(self.music_player.setPosition)
+
+        self.player_position = QTime(0, 0)
+        self.player_duration = QTime(0, 0)
+        self.music_player.durationChanged.connect(self.on_duration_change)
+        self.music_player.positionChanged.connect(self.on_position_change)
+        self.play_time=QLabel(f"{self.player_position.toString('mm:ss')} / {self.player_duration.toString('mm:ss')}",self.player_menu)
         self.play_time.setStyleSheet("color: white")
         self.player_height=50
+
         self.player_menu.setGeometry(0,self.height()-self.player_height,self.width(),self.player_height)
         self.play_button.setGeometry(10,10,30,30)
-        self.play_slider.setGeometry(50,15,self.player_menu.width()-50-10-100,20)
+        self.play_slider.setGeometry(50,15,self.player_menu.width()-50-10-100-40,20)
         self.play_time.setGeometry(50+10+self.play_slider.width(),15,100,20)
         
         self.player_menu.setStyleSheet("background-color: #444")
+        self.isplaying=False
+
+        self.volume_button = QPushButton("🔊", self.player_menu)
+        self.volume_button.setGeometry(self.width() - 40, 10, 30, 30)
+        self.volume_button.setStyleSheet("color: white; font-size: 14px;")
+        self.volume_button.clicked.connect(self.toggle_volume_slider)
+
+        self.volume_slider_frame=QFrame(self)
+        self.volume_slider = QSlider(Qt.Vertical, self.volume_slider_frame)
+        self.volume_slider.setStyleSheet("""
+        QSlider::groove:vertical {
+            background: #666;
+            width: 6px;
+            border-radius: 3px;
+        }
+
+        QSlider::handle:vertical {
+            background: white;
+            border: 1px solid #000;
+            height: 4px;
+            margin: -1px;
+            border-radius: 8px;
+        }
+
+        QSlider::sub-page:vertical {
+            background: #333;
+            border-radius: 3px;
+        }
+
+        QSlider::add-page:vertical {
+            background: #bbb;
+            border-radius: 3px;
+        }
+        """)
+        self.volume_slider.setRange(0, 100)
+        self.volume_slider.setValue(self.music_player.volume())
+        self.volume_slider_frame.setGeometry(self.width() - 40, 10, 34, 120) 
+        self.volume_slider_frame.setStyleSheet("background-color: #666;border-radius:12px;") 
+        self.volume_slider_frame.hide()
+        self.volume_slider.setGeometry(2,10,30,100)
+        self.volume_slider.valueChanged.connect(self.music_player.setVolume)
 
         self.btn_anim=QPropertyAnimation(self.toggle_btn,b"geometry")
         self.btn_anim.setDuration(150)
@@ -208,6 +274,38 @@ class MainWindow(QMainWindow):
         self.menu_anim.setDuration(150)
         
         self.menu_visible=False
+
+    def toggle_play(self):
+        self.isplaying=not (self.isplaying)
+        if(self.isplaying==False):
+            self.music_player.pause()
+            self.play_button.setText("⏵")
+        else:
+            self.music_player.play()
+            self.play_button.setText("⏸")
+            
+    def on_duration_change(self, dur):
+        self.player_duration = QTime(0, 0).addMSecs(dur)
+        self.play_slider.setMaximum(dur)
+        self.update_play_time_label()
+
+    def on_position_change(self, pos):
+        self.player_position = QTime(0, 0).addMSecs(pos)
+        self.play_slider.setValue(pos)
+        self.update_play_time_label()
+    
+    def toggle_volume_slider(self):
+        if self.volume_slider.isVisible():
+            self.volume_slider_frame.hide()
+        else:
+            self.volume_slider_frame.show()
+
+
+    def update_play_time_label(self):
+        current = self.player_position.toString('mm:ss') if hasattr(self, 'player_position') else "00:00"
+        total = self.player_duration.toString('mm:ss') if hasattr(self, 'player_duration') else "00:00"
+        self.play_time.setText(f"{current} / {total}")
+
 
     def toggle_menu(self):
         if self.menu_visible:
@@ -238,8 +336,11 @@ class MainWindow(QMainWindow):
         self.toggle_btn.move(x, int(self.height() / 2) - self.toggle_btn.height() // 2)
         
         self.player_menu.setGeometry(0, self.height() - self.player_height, self.width(), self.player_height)
-        self.play_slider.setGeometry(50,15,self.player_menu.width()-50-10-100,20)
+        self.play_slider.setGeometry(50,15,self.player_menu.width()-50-10-100-40,20)
         self.play_time.setGeometry(50+10+self.play_slider.width(),15,100,20)
+
+        self.volume_button.move(self.width() - 40, 10)
+        self.volume_slider_frame.move(self.width() - 42, self.player_menu.y() - 123)
 
         super().resizeEvent(event)
 
